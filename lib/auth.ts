c8aa689357f0f5f33c2aa577631f5be6f1b8d353
Heejoon Kang <haseo.hackers@gmail.com>
2024-04-20 22:39:22 +0900

    Bearer token

---
+++ lib/auth.ts
@@ -0,0 +1,25 @@
+import { importPKCS8, SignJWT } from 'npm:jose@5.2.4';
+
+const alg = 'ES256';
+const typ = 'JWT';
+const audience = 'appstoreconnect-v1';
+const expirationTime = '20m';
+
+/**
+ * Generates a bearer token for the App Store Connect API.
+ */
+export async function bearerTokenOf({ issuerId, apiKey, privateKey }: {
+  issuerId: string;
+  apiKey: string;
+  privateKey: string;
+}): Promise<string> {
+  const secret = await importPKCS8(privateKey, alg);
+  const jwt = await new SignJWT({})
+    .setProtectedHeader({ kid: apiKey, alg, typ })
+    .setIssuedAt()
+    .setIssuer(issuerId)
+    .setAudience(audience)
+    .setExpirationTime(expirationTime)
+    .sign(secret);
+  return jwt;
+}
